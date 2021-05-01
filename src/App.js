d7435f9bd6db00d1fd9873a0be9b04055ca27a93
=Akshar Vijay <=>
2021-05-01 13:17:32 +1200

    initial HomePage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./Containers/HomePage/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/HomePage/HomePage.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const HomePage = () => {
+  const [posts, setPosts] = useState("");
+
+  let config = { Authorization: "3055f8f90fa44bbe8bda05385a20690a" };
+  const url = "https://newsapi.org/v2/top-headlines?sources=bbc-news";
+
+  useEffect(() => {
+    AllPosts();
+  }, []);
+
+  const AllPosts = () => {
+    axios
+      .get(`${url}`, { headers: config })
+
+      .then((response) => {
+        const allPosts = response.data.articles;
+        console.log(response);
+        setPosts(allPosts);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  };
+
+  return <div>Posts</div>;
+};
+
+export default HomePage;
