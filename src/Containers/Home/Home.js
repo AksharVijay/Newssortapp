a33b6d6a6211c15092c2cbcd5b577947caafec69
=Akshar Vijay <=>
2021-05-02 20:55:30 +1200

    sorting and css

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Posts from "../../Components/Posts/Posts";
 import axios from "axios";
+import { Fragment } from "react";
+import "./Home.css";
 
 const Home = () => {
   const [posts, setPosts] = useState("");
+  const [sortBy, setSortBy] = useState("none");
 
   let config = { Authorization: "3055f8f90fa44bbe8bda05385a20690a" };
   const url = "https://newsapi.org/v2/top-headlines?sources=bbc-news";
@@ -25,9 +28,32 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <Posts className="Posts" posts={posts} />
-    </div>
+    <Fragment>
+      <div className="home">
+        <div className="select">
+          <select
+            name="slct"
+            id="slct"
+            onChange={(e) => setSortBy(e.target.value)}
+          >
+            <option value="title">Title</option>
+            <option value="urlToImage">Image</option>
+            <option value="author">Author</option>
+            <option value="description">Description</option>
+            <option value="none">No Sort</option>
+          </select>
+        </div>
+        <Posts
+          className="Posts"
+          posts={
+            sortBy === "none"
+              ? posts
+              : posts.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+          }
+          key={posts.title}
+        />
+      </div>
+    </Fragment>
   );
 };
 
